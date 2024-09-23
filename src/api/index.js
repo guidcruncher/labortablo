f817cec4374adea2c9835ebf15d1e710942ac498
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2024-09-23 12:20:59 +0100

    allowing Service preload to not block API server

--- src/api/index.js
+++ src/api/index.js
@@ -16,18 +16,23 @@ fastify.register(require("./routes/repository.js"));
 fastify.register(require("./routes/icon.js"));
 fastify.register(require("./routes/bookmarks.js"));
 
-fastify.log.info("Performing pre-load");
-docker.preload().then(() => {
-  fastify.log.info("Pre-load finished");
+// Run the server!
+const listenPort = process.env.API_PORT || 9080;
+const listenAddress = process.env.API_LISTEN_ADDRESS || "0.0.0.0";
+fastify.listen({ port: listenPort, host: listenAddress }, (err, address) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`API Server listening on ${address}`);
 
-  // Run the server!
-  const listenPort = process.env.API_PORT || 9080;
-  const listenAddress = process.env.API_LISTEN_ADDRESS || "0.0.0.0";
-  fastify.listen({ port: listenPort, host: listenAddress }, (err, address) => {
-    if (err) {
-      fastify.log.error(err);
-      process.exit(1);
-    }
-    fastify.log.info(`API Server listening on ${address}`);
-  });
+  fastify.log.info("Performing service preload...");
+  docker
+    .preload()
+    .then(() => {
+      fastify.log.info("Service preload finished.");
+    })
+    .catch(() => {
+      fastify.log.error("Something went wrong with the Service Preload");
+    });
 });
