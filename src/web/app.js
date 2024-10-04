a669a538d67153ad72fde0269cf4b3e439e41dec
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2024-10-04 04:36:21 +0100

    dix

--- src/web/app.js
+++ src/web/app.js
@@ -1,4 +1,4 @@
-		const express = require("express");
+const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const helpers = require("./helpers/helpers.js");
