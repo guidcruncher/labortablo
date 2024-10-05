3cf708cd9a54c1061b090d48f5071e04d4fddca4
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2024-10-05 02:28:31 +0100

    Systeminfo

--- src/services/systeminfo.js
+++ src/services/systeminfo.js
@@ -2,7 +2,17 @@ const si = require("systeminformation");
 
 function gather() {
   return new Promise((resolve, reject) => {
-    si.cpu()
+    var valueObject = {
+      cpu: "manufacturer, brand, vendor, family, speed, speedMin, speedMax, governor, socket",
+      cpuTemperature: "main, max, chipset",
+      cpuCurrentSpeed: "avg, min, max",
+      mem: "total, free, used, swaptotal, swapfree, swapused",
+      fsSize: "*",
+      osInfo: "platform, distro, codename, release",
+      system: "model, manufacturer, raspberry",
+    };
+
+    si.get(valueObject)
       .then((data) => {
         resolve(data);
       })
